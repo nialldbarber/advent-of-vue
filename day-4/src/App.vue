5b9cbd82e67e9f17e3941a86395efa835675a186
Niall Barber <niall.barber@olioex.com>
2022-12-06 07:40:24 +0000

    Reshuffle

---
+++ day-4/src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import ChristmasTree from './ChristmasTree.vue'
+</script>
+
+<template>
+  <div class="w-full h-full flex justify-center items-center">
+    <ChristmasTree :size="7" />
+  </div>
+</template>
