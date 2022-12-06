b6753674715fd3746a5db42abbbdd007313728e1
Niall Barber <niall.barber@olioex.com>
2022-12-06 08:17:02 +0000

    Day 5

--- day-5/src/App.vue
+++ day-5/src/App.vue
@@ -1,3 +1,11 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import Input from './components/Input.vue'
+
+const to = ref('')
+const from = ref('')
+</script>
+
 <template>
   <div class="w-full h-full flex justify-center items-center">
     <div>
@@ -6,15 +14,13 @@
           <img src="/assets/santa.svg" alt="" class="max-w-[12em]" />
           <div>
             <h1 class="font-serif text-4xl">Happy Holidays!</h1>
-            <div class="mt-4 bg-white text-black p-2 rounded">TO: John</div>
-            <div class="mt-2 bg-white text-black p-2 rounded">FROM: Joana</div>
+            <div class="mt-4 bg-white text-black p-2 rounded">TO: {{ to }}</div>
+            <div class="mt-2 bg-white text-black p-2 rounded">FROM: {{ from }}</div>
           </div>
         </div>
       </div>
+      <Input v-model="to" label="TO" />
+      <Input v-model="from" label="FROM" />
     </div>
   </div>
 </template>
-
-<script setup>
-// ...
-</script>
---
+++ day-5/src/components/Input.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+interface Props {
+  modelValue: string
+  label: string
+}
+
+interface Emits {
+  (e: 'change', modelValue: string): void
+  (e: 'update', modelValue: string): void
+}
+
+const { modelValue } = defineProps<Props>()
+const emit = defineEmits<Emits>()
+</script>
+
+<template>
+  <label class="block">
+    {{ label }}:
+    <input
+      class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2"
+      type="text"
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+    />
+  </label>
+</template>
